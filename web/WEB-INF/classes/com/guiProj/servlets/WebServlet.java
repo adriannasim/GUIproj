5536cf4e260dd5d7dd0cf25d890003db88f0f47d
adriannasim <adriannasim@gmail.com>
2024-04-28 02:44:47 +0800

    testing

---
+++ web/WEB-INF/classes/com/guiProj/servlets/WebServlet.java
@@ -0,0 +1,15 @@
+package com.guiProj.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class WebServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Forwards request to home.jsp
+        RequestDispatcher dispatcher = request.getRequestDispatcher("web/home.jsp");
+        dispatcher.forward(request, response);
+    }
+}
